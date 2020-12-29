cece8d5cc551c7a8ed816333cab5b020f62b2e3e
Elton Shi <elton.shi@slicejobs.com.cn>
2020-12-29 17:51:00 +0800

    完善项目目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <mainTabBar></mainTabBar>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+
+  import mainTabBar from './components/content/mainTabbar/MainTabBar'
+  export default {
+    name: 'app',
+    components: {
+      mainTabBar: mainTabBar
+    }
+  }
+</script>
+
+<style>
+  @import 'assets/css/base.css';
 </style>
